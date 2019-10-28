47708f2d19c1a5ea67f8d413fe1efb7e68cede60
cristine <christinekhong8@gmail.com>
2019-10-28 12:59:05 +0700

    mengubah connection.php

--- connection.php
+++ connection.php
@@ -1,23 +1,93 @@
-<?php  
-$serverName = "10.100.70.70\\AKREDITASI2020";
-$uid = "guestManPro";
-$pwd = "Testing123";
-$databaseName = "Akreditasi2020";
-$connectionInfo = array( "UID"=>$uid,                            
-                        "PWD"=>$pwd,                            
-                        "Database"=>$databaseName); 
-/* Connect using SQL Server Authentication. */  
-$conn = sqlsrv_connect($serverName, $connectionInfo); 
-$query = "EXEC Tabel3a2_DosenPembimbingUtamaTugasAkhir;";
-/* Execute the query. */ 
-$stmt = sqlsrv_query( $conn, $query);  
-if($stmt){
-    echo "Statement executed.<br>\n";
-}   
-else{
-    echo "Error in statement execution.\n";  
-    die(print_r( sqlsrv_errors(), true));  
+<?php
+/**
+ * Kelas untuk membuka koneksi ke Server dan mengakses database
+ */
+class SQL
+{
+	public $db_connection;			
+	function __construct()
+	{
+		$this->servername = "10.100.70.70\\AKREDITASI2020";
+		$this->username = "guestManPro";
+		$this->password  = "Testing123";
+		$this->dbname = "Akreditasi2020";
+		$this->db_connection_info = array(
+			"UID" => $this->username,
+			"PWD" => $this->password,
+			"Database" => $this->dbname
+		);
+	}
+	/** 
+	 * Function untuk memulai koneksi ke server untuk mengakses database
+	 * @return	bool|(bool|mixed)[]	Kembalian berupa boolean true apabila koneksi berhasil tersambung, atau
+	 * 									kembalian berupa array yang berisi boolean(false) dan pesan error
+	 */
+	function openConnection()
+	{
+		$this->db_connection = sqlsrv_connect($this->servername, $this->db_connection_info);
+		$success = true;
+		if ($this->db_connection) {
+			//echo "Connection established.<br />";
+			return $success;
+		} else {
+			// echo "Connection could not be established.<br />";
+			// die(print_r(sqlsrv_errors(), true));
+			return [$success, "message"=>sqlsrv_errors()];
+		}
+	}
+	/** 
+	 *Function untuk memanggil stored procedure yang ada pada Database
+	 * 	@param	string	$query	Query eksekusi stored procedure yang ingin dijalankan
+	 *	@param	array	$param	Parameter yang akan disertakan sesuai dengan query
+	 *
+	 *	@return array	Tabel hasil eksekusi query
+	 */
+	function executeStoredProcedure($query, $param)
+	{
+		$this->openConnection();
+		$nocount = sqlsrv_query($this->db_connection, "SET NOCOUNT ON");
+		$statement = sqlsrv_prepare($this->db_connection, $query, $param);
+		$query_result = sqlsrv_execute($statement);
+		$result = [];
+		while ($row = sqlsrv_fetch_array($statement, SQLSRV_FETCH_BOTH)) {
+			if ($row === false) {
+				if (($errors = sqlsrv_errors()) != null) {
+					foreach ($errors as $error) {
+						echo "SQLSTATE: " . $error['SQLSTATE'] . "<br />";
+						echo "code: " . $error['code'] . "<br />";
+						echo "message: " . $error['message'] . "<br />";
+					}
+				}
+			}
+			$result[] = $row;
+		}
+		sqlsrv_close($this->db_connection);
+		return $result;
+	}
+	/**
+	 * 	@param	string	$query	Query select yang akan dijalankan
+	 *	@param	array	$param	Parameter yang akan disertakan sesuai dengan query
+	 *
+	 *	@return array	Tabel hasil eksekusi query 
+	 */
+	function executeSelectQuery($query, $param)
+	{
+		$this->openConnection();
+		$statement = sqlsrv_prepare($this->db_connection, $query, $param);
+		$query_result = sqlsrv_execute($statement);
+		while ($row = sqlsrv_fetch_array($statement, SQLSRV_FETCH_NUMERIC)) {
+			if ($row === false) {
+				if (($errors = sqlsrv_errors()) != null) {
+					foreach ($errors as $error) {
+						echo "SQLSTATE: " . $error['SQLSTATE'] . "<br />";
+						echo "code: " . $error['code'] . "<br />";
+						echo "message: " . $error['message'] . "<br />";
+					}
+				}
+			}
+			$result[] = $row;
+		}
+		sqlsrv_close($this->db_connection);
+		return $result;
+	}
 }
-sqlsrv_free_stmt( $stmt);  
-sqlsrv_close( $conn);  
-?>

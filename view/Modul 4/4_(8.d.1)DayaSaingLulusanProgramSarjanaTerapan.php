318e84a84931217d39b6085c74af0d6cc5ce0061
Yulius Famas <yuliusfamas@gmail.com>
2019-10-30 21:36:20 +0700

    8.c Doktor

---
+++ view/Modul 4/4_(8.d.1)DayaSaingLulusanProgramSarjanaTerapan.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-flat.css">
+    <title>Daya Saing Lulusan Program Sarjana Terapan</title>
+    <style>
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+        th, td {
+            padding: 5px;
+        }
+        th{
+            text-align: center;
+        }
+        table{
+            margin-left: 20%;
+            margin-right: 20%;
+            width: 60%;
+            margin-top: 7%;
+        }
+        h1{
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h1><b>Daya Saing Lulusan Program Sarjana Terapan</b></h1>
+    <table>
+        <tr>
+            <th class="w3-flat-belize-hole" rowspan="2">Tahun Lulus</th>
+            <th class="w3-flat-belize-hole" rowspan="2">Jumlah Lulusan</th>
+            <th class="w3-flat-belize-hole" rowspan="2">Jumlah <br>Lulusan <br>yang <br>Terlacak</th>
+            <th class="w3-flat-belize-hole" colspan="3">Jumlah Lulusan Terlacak <br>
+                                            dengan Waktu Tunggu <br>
+                                            Mendapatkan Pekerjaan</th>
+        </tr>
+        <tr>
+            <th class="w3-flat-belize-hole">WT<3 <br>bulan  </th>
+            <th class="w3-flat-belize-hole">3<=WT<= <br>6 bulan  </th>
+            <th class="w3-flat-belize-hole">WT>6 <br>bulan  </th>    
+        </tr>
+        <tr>
+            <th>1</th>
+            <th>2</th>
+            <th>3</th>
+            <th>4</th>
+            <th>5</th>
+            <th>6</th>
+        </tr>
+        <tr>
+        <tr>
+            <td>TS-4</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>TS-3</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+         <tr>
+            <td>TS-2</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+         </tr>
+    </table>
+</body>
+</html>

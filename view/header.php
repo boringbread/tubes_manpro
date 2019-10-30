e7d613b00f9452602bf963a58288302e3217dacf
cristine <christinekhong8@gmail.com>
2019-10-30 21:21:52 +0700

    Update header.php
    mengarahkan navbar ke tab halamanProfileDosen

--- view/header.php
+++ view/header.php
@@ -84,7 +84,7 @@
                 <div class="w3-dropdown-hover w3-hide-small">
                   <button class="w3-padding-large w3-button" title="More">SUMBER DAYA MANUSIA <i class="fa fa-caret-down"></i></button>
                   <div class="w3-dropdown-content w3-bar-block w3-card-4">
-                      <a href="#" class="w3-bar-item w3-button">PROFIL DOSEN</a>
+                      <a href="halamanProfileDosen.php" class="w3-bar-item w3-button">PROFIL DOSEN</a>
                       <a href="#" class="w3-bar-item w3-button">KINERJA DOSEN</a>
                   </div>
                 </div>

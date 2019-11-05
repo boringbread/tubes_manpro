9e97ad9724b627119c6f7984e1ecd80348a2fae8
Yulius Famas <yuliusfamas@gmail.com>
2019-11-05 13:55:32 +0700

    ADD- 8.b.2

---
+++ view/Modul 4/4_PrestasiNonAkademikMahasiswa.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-flat.css">
+    <title>Prestasi Nonakademik</title>
+    <style>
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+        th, td {
+            padding: 5px;
+        }
+        th{
+            text-align: center;
+        }
+        table{
+            margin-left: 20%;
+            margin-right: 20%;
+            width: 60%;
+            margin-top: 7%;
+        }
+        h1{
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h1><b>Prestasi Nonakademik</b></h1>
+    <table>
+        <tr>
+            <th class="w3-flat-belize-hole" rowspan="2">No</th>
+            <th class="w3-flat-belize-hole" rowspan="2">Nama Kegiatan</th>
+            <th class="w3-flat-belize-hole" rowspan="2">Tahun Perolehan</th>
+            <th class="w3-flat-belize-hole" colspan="3">Tingkat</th>
+            <th class="w3-flat-belize-hole" rowspan="2">Prestasi yang Dicapai</th>
+        </tr>
+        <tr>
+            <th class="w3-flat-belize-hole">Lokal/Wilayah</th>
+            <th class="w3-flat-belize-hole">Nasional</th>
+            <th class="w3-flat-belize-hole">Internasional</th>
+        </tr>
+        <tr>
+            <th>1</th>
+            <th>2</th>
+            <th>3</th>
+            <th>4</th>
+            <th>5</th>
+            <th>6</th>
+            <th>7</th>
+        </tr>
+        <tr>
+            <td> </td>
+            <td> </td>
+            <td> </td>
+            <td> </td>
+            <td> </td> 
+            <td> </td>
+            <td> </td>
+        </tr>
+        <tr>
+            <td colspan="3" style="text-align: center;"><b> Jumlah</b></td>
+            <td> </td>
+            <td> </td>
+            <td> </td>
+            <td class="w3-flat-belize-hole"> </td> 
+        </tr>
+    </table>
+</body>
+</html>

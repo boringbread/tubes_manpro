704b1ada3cdd21ec2b5710a76e8e0691d8af6742
Steren <43349985+Sterennn@users.noreply.github.com>
2019-11-05 14:04:08 +0700

    Add files via upload

--- view/Modul 4/4_(8.e.1)tempatKerjaLulusan.php
+++ view/Modul 4/4_(8.e.1)tempatKerjaLulusan.php
@@ -1,52 +1,30 @@
+<?php include '../header.php' ?>
+<style type="text/css">
+    body{
+        color: white;
+    }
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Tabel 8.e.1) Tempat Kerja Lulusan</title>
-<style>
-    table,th, td {
-        border: 1px solid black;
-        border-collapse: collapse;
-  		text-align:center;
-  		
-      }
-  
-  	th[rowspan="2"]{
-  		background-color: lightgrey;
-  		padding:40px;
-  	}
-  th[colspan="3"]{
-  		background-color: lightgrey;
-  		padding:25px;
-  	}
-    </style>
-</head>
+    table, th, td{
+        border: 1px solid white;
+    }
+
+
+</style>
 <body>
     <table>
         <tr>
             <th rowspan="2">Tahun Lulus</th>
             <th rowspan="2">Jumlah Lulusan</th>
           <th rowspan="2">Jumlah Lulusan yang Terlacak</th>
-            <th colspan="3">Jumlah Lulusan Terlacak yang Bekerja
-berdasarkan Tingkat/Ukuran Tempat
-Kerja/Berwirausaha</th>
+            <th colspan="3">Jumlah Lulusan Terlacak yang Bekerja<br> berdasarkan Tingkat/Ukuran Tempat<br> Kerja/Berwirausaha</th>
         </tr>
 
        
 
         </tr>
-            <td style="background-color:lightgrey;"><b>Lokal/
-Wilayah/
-Berwirausaha
-tidak Berizin</td></b>
-            <td  style="background-color:lightgrey;"><b>Nasional/
-Berwirausaha
-Berizin</td></b>
-            <td  style="background-color:lightgrey;"><b>Multinasiona/
-Internasional</td></b>
+            <td><b>Lokal/<br> Wilayah/<br> Berwirausaha<br> tidak Berizin</td></b>
+            <td><b>Nasional/<br> Berwirausaha<br> Berizin</td></b>
+            <td><b>Multinasiona/<br> Internasional</td></b>
         </tr>
 
         <tr>
@@ -82,7 +60,7 @@ Internasional</td></b>
                 <td> </td>
                 <td> </td>
                 <td> </td>
-                <th </th>
+                <th> </th>
         </tr>
 
 

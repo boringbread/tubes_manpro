439ad0a3e37659f1af32c3c218a9b1ed8ce28d80
Steren <43349985+Sterennn@users.noreply.github.com>
2019-11-05 16:17:32 +0700

    Add files via upload

--- view/Modul 4/4_(8.f.1)_tabel2_pagelaran.php
+++ view/Modul 4/4_(8.f.1)_tabel2_pagelaran.php
@@ -1,9 +1,20 @@
+<?php include '../header.php' 
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+    
+        <title>Integrasi Kegiatan Penelitian</title>
+    
+        <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+        <link href="../Modul 3/css/sb-admin-2.min.css" rel="stylesheet">
+        <link href="../Modul 3/css/customStyle.css" rel="stylesheet" type="text/css">
     <title>Tabel 8.f.1) Pagelaran/pameran/presentasi/publikasi Ilmiah mahasiswa</title>
 <style>
     table,th, td {
@@ -156,3 +167,4 @@ dalam forum di tingkat internasional</th>
    
 </body>
 </html>
+<?php include '../footer.php' ?>

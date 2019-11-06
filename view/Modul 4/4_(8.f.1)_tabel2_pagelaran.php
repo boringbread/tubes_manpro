5a4a5f7e69fd6d9677795ba2bf980238ca89d3c9
Steren <43349985+Sterennn@users.noreply.github.com>
2019-11-06 14:49:06 +0700

    [ADD] Data to table

---
+++ view/Modul 4/4_(8.f.1)_tabel2_pagelaran.php
@@ -0,0 +1,195 @@
+<?php include '../header.php' 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+    
+        <title>Integrasi Kegiatan Penelitian</title>
+    
+        <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+        <link href="../Modul 3/css/sb-admin-2.min.css" rel="stylesheet">
+        <link href="../Modul 3/css/customStyle.css" rel="stylesheet" type="text/css">
+    <title>Tabel 8.f.1) Pagelaran/pameran/presentasi/publikasi Ilmiah mahasiswa</title>
+<style>
+    table,th, td {
+        border: 1px solid black;
+        border-collapse: collapse;
+  		text-align:center;
+      }
+  
+  	th[rowspan="2"]{
+  		background-color: lightgrey;
+  	}
+  th[colspan="3"]{
+  		background-color: lightgrey;
+  	}
+    </style>
+</head>
+<body>
+    <table>
+        <tr>
+            <th rowspan="1">jenisPublikasi</th>
+            <th colspan="1">ts2</th>
+            <th rowspan="1">ts1</th>
+            <th rowspan="1">ts</th>
+            <th rowspan="1">jumlah</th>
+        </tr>
+
+       
+
+        <!-- </tr>
+            <td style="background-color:lightgrey;"><b>TS-2</td></b>
+            <td  style="background-color:lightgrey;"><b>TS-1</td></b>
+            <td  style="background-color:lightgrey;"><b>TS</td></b>
+        </tr>
+
+        <tr>
+            <th  style="background-color:lightgrey;">1</th>
+            <th style="background-color:lightgrey;">2</th>
+            <th  style="background-color:lightgrey;">3</th>
+            <th  style="background-color:lightgrey;">4</th>
+            <th  style="background-color:lightgrey;">5</th>
+            <th  style="background-color:lightgrey;">6</th>
+        </tr>
+
+        <tr>
+                <th>1</th>
+                <th style="text-align: left">Publikasi di jurnal nasional tidak
+terakreditasi</th>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <th>N<sub>A1</sub> =  </th>
+        </tr>
+
+        <tr>
+                <th>2</th>
+                <th style="text-align: left">Publikasi di jurnal nasional
+terakreditasi</th>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <th>N<sub>A2</sub> =  </th>
+        </tr>
+
+        <tr>
+                <th>3</th>
+                <th style="text-align: left">Publikasi di jurnal internasional </th>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <th>N<sub>A3</sub> =  </th>
+        </tr>
+
+        <tr>
+                <th>4</th>
+                <th style="text-align: left">Publikasi di jurnal internasional
+bereputasi</th>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <th>N<sub>A4</sub> =  </th>
+        </tr>
+
+        <tr>
+                <th>5</th>
+                <th style="text-align: left">Publikasi di seminar
+wilayah/lokal/perguruan tinggi</th>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <th>N<sub>B1</sub> =  </th>
+        </tr>
+
+        <tr>
+                <th>6</th>
+                <th style="text-align: left">Publikasi di seminar nasional</th>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <th>N<sub>B2</sub> =  </th>
+        </tr>
+        
+        <tr>
+                <th>7</th>
+                <th style="text-align: left">Publikasi di seminar internasional</th>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <th>N<sub>B3</sub> =  </th>
+        </tr>
+
+        <tr>
+                <th>8</th>
+                <th style="text-align: left">Pagelaran/pameran/presentasi
+dalam forum di tingkat wilayah</th>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <th>N<sub>C1</sub> =  </th>
+        </tr>
+
+        <tr>
+                <th>9</th>
+                <th style="text-align: left">Pagelaran/pameran/presentasi
+dalam forum di tingkat nasional</th>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <th>N<sub>C2</sub> =  </th>
+        </tr>
+
+        <tr>
+                <th>10</th>
+                <th style="text-align: left">Pagelaran/pameran/presentasi
+dalam forum di tingkat internasional</th>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <th>N<sub>C3</sub> =  </th>
+        </tr>
+
+    <tr>
+        <th colspan=2>Jumlah</th>
+        <th> </th>
+        <th> </th>
+        <th> </th>
+        <th> </th>
+
+    </tr> -->
+    <?php
+                include "../../connection.php";
+
+                $temp = new SQL();
+                $query = "EXEC [Tabel8f1_PartSeminarMahasiswa]";
+                $res = $temp->executeStoredProcedure($query,[]);
+
+                $counter = 0;
+                for($x=0; $x<sizeof($res); $x++){
+                    echo "<tr>";
+                    echo "<td>".$res[$x][$counter]."</td>";
+                    $counter++;
+                    echo "<td>".$res[$x][$counter]."</td>";
+                    $counter++;
+                    echo "<td>".$res[$x][$counter]."</td>";
+                    $counter++;
+                    echo "<td>".$res[$x][$counter]."</td>";
+                    $counter++;
+                    echo "<td>".$res[$x][$counter]."</td>";
+                    $counter++;
+                    echo "</tr>";
+                    $counter=0;
+                }
+            ?>
+
+    </table>
+   
+</body>
+</html>
+<?php include '../footer.php' ?>

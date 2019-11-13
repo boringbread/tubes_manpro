2b9303df7b2d408ff5afb2deaf9834ebd130b7cf
friskac <54855152+friskac@users.noreply.github.com>
2019-11-13 21:11:42 +0700

    some text fixed

--- view/Modul 2/halamanKinerjaDosen.php
+++ view/Modul 2/halamanKinerjaDosen.php
@@ -18,150 +18,143 @@
     </style>
 <body>
     
-
     <h1 class="w3-border-buttom">Kinerja Dosen</h1>
-    <div class="w3-container">
-        <h4>Tabel 3.b.1) Pengakuan/Rekognisi DTPS</h4>
-        <h1 class="w3-border-buttom">Kinerja Dosen</h1>
-        <button onclick="showcount('tabel3b1')" class="w3-button w3-block w3-left-align">
+    <button onclick="showCont('tabel3b1')" class="w3-button w3-block w3-left-align">
         Tabel 3.b.1) Pengakuan/Rekognisi DTPS
         <div style="overflow-x:auto" id="tabel3b1" class="w3-container w3-hide">
-        <table>
-            <tr>
-                <th rowspan="2">No.</th>
-                <th rowspan="2">Nama Dosen</th>
-                <th rowspan="2">Bidang Keahlian</th>
-                <th rowspan="2">Rekognisi dan Bukti Pendukung</th>
-                <th colspan="3">Tingkat</th>
-                <th rowspan="2">Tahun</th>
-            </tr>
-            <tr>
-                <th>Wilayah</th>
-                <th>Nasional</th>
-                <th>Internasional</th>
-            </tr>
-            <?php
-                include "../../connection.php";
-                $temp = new SQL();
-                $query = "EXEC Tabel3b1_RekognisiDTPS";
-                $res = $temp->executeStoredProcedure($query,[]);
-                $index = 1;
-                $counter = 0;
-                for($x=0; $x<sizeof($res); $x++){
-                    echo "<tr>";
-                    // echo "<td>".$index."</td>";
-                    // $index++;
-                    echo "<td>".$res[$x][$counter]."</td>";
-                    $counter++;
-                    echo "<td>".$res[$x][$counter]."</td>";
-                    $counter++;
-                    echo "<td>".$res[$x][$counter]."</td>";
-                    $counter++;
-                    echo "<td>".$res[$x][$counter]."</td>";
-                    $counter++;
-                    echo "<td>".$res[$x][$counter]."</td>";
-                    $counter++;
-                    echo "<td>".$res[$x][$counter]."</td>";
-                    $counter++;
-                    echo "<td>".$res[$x][$counter]."</td>";
-                    $counter++;
-                    echo "<td>".$res[$x][$counter]."</td>";
-                    $counter=0;
-                    echo "</tr>";
-                }
-            ?>
-        </table>
-    </div>
+            <table>
+                <tr>
+                    <th rowspan="2">No.</th>
+                    <th rowspan="2">Nama Dosen</th>
+                    <th rowspan="2">Bidang Keahlian</th>
+                    <th rowspan="2">Rekognisi dan Bukti Pendukung</th>
+                    <th colspan="3">Tingkat</th>
+                    <th rowspan="2">Tahun</th>
+                </tr>
+                <tr>
+                    <th>Wilayah</th>
+                    <th>Nasional</th>
+                    <th>Internasional</th>
+                </tr>
+                <?php
+                    include "../../connection.php";
+                    $temp = new SQL();
+                    $query = "EXEC Tabel3b1_RekognisiDTPS";
+                    $res = $temp->executeStoredProcedure($query,[]);
+                    $index = 1;
+                    $counter = 0;
+                    for($x=0; $x<sizeof($res); $x++){
+                        echo "<tr>";
+                        // echo "<td>".$index."</td>";
+                        // $index++;
+                        echo "<td>".$res[$x][$counter]."</td>";
+                        $counter++;
+                        echo "<td>".$res[$x][$counter]."</td>";
+                        $counter++;
+                        echo "<td>".$res[$x][$counter]."</td>";
+                        $counter++;
+                        echo "<td>".$res[$x][$counter]."</td>";
+                        $counter++;
+                        echo "<td>".$res[$x][$counter]."</td>";
+                        $counter++;
+                        echo "<td>".$res[$x][$counter]."</td>";
+                        $counter++;
+                        echo "<td>".$res[$x][$counter]."</td>";
+                        $counter++;
+                        echo "<td>".$res[$x][$counter]."</td>";
+                        $counter=0;
+                        echo "</tr>";
+                    }
+                ?>
+            </table>
+        </div>
     </button>
 
-    <div class="tabel3b2">
-    <h4>3.b.2) Penelitian DTPS</h4>
-    <button onclick="showcount('tabel3b2')" class="w3-button w3-block w3-left-align">
+    <button onclick="showCont('tabel3b2')" class="w3-button w3-block w3-left-align">
         Tabel 3.b.2) Penelitian DTPS
         <div style="overflow-x:auto" id="tabel3b2" class="w3-container w3-hide">
-        <table>
-            <tr>
-                <th rowspan="2">No.</th>
-                <th rowspan="2">Sumber Pembiayaan</th>
-                <th colspan="3">Jumlah Judul</th>
-                <th rowspan="2">Jumlah</th>
-            </tr>
-            <tr>
-                    <th>TS-2</th>
-                    <th>TS-1</th>
-                    <th>TS</th>
-            </tr>
-            <?php
-                $temp = new SQL();
-                $query = "EXEC Tabel3b2_PenelitianDTPS";
-                $res = $temp->executeStoredProcedure($query,[]);
-                $counter = 0;
-                for($x=0; $x<sizeof($res); $x++){
-                    echo "<tr>";
-                    echo "<td>".$res[$x][$counter]."</td>";
-                    $counter++;
-                    echo "<td>".$res[$x][$counter]."</td>";
-                    $counter++;
-                    echo "<td>".$res[$x][$counter]."</td>";
-                    $counter++;
-                    echo "<td>".$res[$x][$counter]."</td>";
-                    $counter++;
-                    // echo "<td>".$res[$x][$counter]."</td>";
-                    // $counter++;
-                    // echo "<td>".$res[$x][$counter]."</td>";
-                    // $counter++;
-                    echo "<td>".$res[$x][$counter]."</td>";
-                    $counter=0;
-                    echo "</tr>";
-                }
-            ?>
-        </table>
-    </div>
+            <table>
+                <tr>
+                    <th rowspan="2">No.</th>
+                    <th rowspan="2">Sumber Pembiayaan</th>
+                    <th colspan="3">Jumlah Judul</th>
+                    <th rowspan="2">Jumlah</th>
+                </tr>
+                <tr>
+                        <th>TS-2</th>
+                        <th>TS-1</th>
+                        <th>TS</th>
+                </tr>
+                <?php
+                    $temp = new SQL();
+                    $query = "EXEC Tabel3b2_PenelitianDTPS";
+                    $res = $temp->executeStoredProcedure($query,[]);
+                    $counter = 0;
+                    for($x=0; $x<sizeof($res); $x++){
+                        echo "<tr>";
+                        echo "<td>".$res[$x][$counter]."</td>";
+                        $counter++;
+                        echo "<td>".$res[$x][$counter]."</td>";
+                        $counter++;
+                        echo "<td>".$res[$x][$counter]."</td>";
+                        $counter++;
+                        echo "<td>".$res[$x][$counter]."</td>";
+                        $counter++;
+                        // echo "<td>".$res[$x][$counter]."</td>";
+                        // $counter++;
+                        // echo "<td>".$res[$x][$counter]."</td>";
+                        // $counter++;
+                        echo "<td>".$res[$x][$counter]."</td>";
+                        $counter=0;
+                        echo "</tr>";
+                    }
+                ?>
+            </table>
+        </div>
     </button>
-        <h4>3.b.3) PKM DTPS</h4>
-    <div class="tabel3b3">
-    <button onclick="showcount('tabel3b3')" class="w3-button w3-block w3-left-align">
+
+    <button onclick="showCont('tabel3b3')" class="w3-button w3-block w3-left-align">
         Tabel 3.b.3) PKM DTPS
         <div style="overflow-x:auto" id="tabel3b3" class="w3-container w3-hide">
-    <table>
-            <tr>
-                <th rowspan="2">No.</th>
-                <th rowspan="2">Sumber Pembayaran</th>
-                <th colspan="3">Jumlah Judul</th>
-                <th rowspan="2">judul</th>
-            </tr>
-            <tr>
-                <th>TS-2</th>
-                <th>TS-3</th>
-                <th>TS</th>
-            </tr>
-            
-            <?php
-            $temp = new SQL();
-            $query = "EXEC tabel3b3_PKMDTPS";
-            $res = $temp->executeStoredProcedure($query,[]);
-            $index = 1;
-            $counter = 0;
-            for($x=0; $x<sizeof($res); $x++){
-                echo "<tr>";
-                echo "<td>".$index."</td>";
-                $index++;
-                echo "<td>".$res[$x][$counter]."</td>";
-                $counter++;
-                echo "<td>".$res[$x][$counter]."</td>";
-                $counter++;
-                echo "<td>".$res[$x][$counter]."</td>";
-                $counter++;;
-                echo "<td>".$res[$x][$counter]."</td>";
-                $counter++;
-                echo "<td>".$res[$x][$counter]."</td>";
-                $counter++;
-                echo "</tr>";
-                $counter=0;
-            }
-            ?>`
-        </table>
-    </div>
+            <table>
+                <tr>
+                    <th rowspan="2">No.</th>
+                    <th rowspan="2">Sumber Pembayaran</th>
+                    <th colspan="3">Jumlah Judul</th>
+                    <th rowspan="2">judul</th>
+                </tr>
+                <tr>
+                    <th>TS-2</th>
+                    <th>TS-3</th>
+                    <th>TS</th>
+                </tr>
+                
+                <?php
+                    $temp = new SQL();
+                    $query = "EXEC tabel3b3_PKMDTPS";
+                    $res = $temp->executeStoredProcedure($query,[]);
+                    $index = 1;
+                    $counter = 0;
+                    for($x=0; $x<sizeof($res); $x++){
+                        echo "<tr>";
+                        echo "<td>".$index."</td>";
+                        $index++;
+                        echo "<td>".$res[$x][$counter]."</td>";
+                        $counter++;
+                        echo "<td>".$res[$x][$counter]."</td>";
+                        $counter++;
+                        echo "<td>".$res[$x][$counter]."</td>";
+                        $counter++;;
+                        echo "<td>".$res[$x][$counter]."</td>";
+                        $counter++;
+                        echo "<td>".$res[$x][$counter]."</td>";
+                        $counter++;
+                        echo "</tr>";
+                        $counter=0;
+                    }
+                ?>
+            </table>
+        </div>
     </button>
 
     <div class="w3-container">

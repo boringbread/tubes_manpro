4746ae6e009e4596ffc02191641cd7d8d661baa9
obolsuro <obolsuro@gmail.com>
2019-11-18 18:33:52 +0700

    tambah data
    nambahin data di table daya saing lulusan

--- view/Modul 4/4_Efektifitas.php
+++ view/Modul 4/4_Efektifitas.php
@@ -3,6 +3,12 @@
     body{
         color:white;
     }
+
+    #dayasaing table{
+        border:white solid 1px;
+        text-align: center;
+        
+    }
 </style>
     <h1><b>Masa Studi Lulusan Program Diploma Tiga</b></h1>
     <table>
@@ -101,7 +107,7 @@
     <br><br>
 
     <h1><b>Masa Studi Lulusan Program Diploma Tiga</b></h1>
-    <table>
+    <table >
         <tr>
             <th class="w3-flat-belize-hole" rowspan="2">Tahun Masuk</th>
             <th class="w3-flat-belize-hole" rowspan="2">Jumlah Mahasiswa Diterima <sup>1)</sup></th>
@@ -152,11 +158,12 @@
         </tr>
     </table>
 
-    <h1><b>Daaya Saing Lulusan Program Diploma Tiga</b></h1>
+    <div id="dayasaing">
+    <h1><b>Daya Saing Lulusan Program Diploma Tiga</b></h1>
     <table>
         <tr>
             <th class="w3-flat-belize-hole" rowspan="2">Tahun Lulus</th>
-            <th class="w3-flat-belize-hole" rowspan="2">Jumlah lulusan </th>row
+            <th class="w3-flat-belize-hole" rowspan="2">Jumlah lulusan </th>
             <th class="w3-flat-belize-hole" rowspan="2">Jumlah Lulusan Terlacak </th>
             <th class="w3-flat-belize-hole" rowspan="2">Jumlah Lulusan yang Dipesan Sebelum Lulus </th>
             <th class="w3-flat-belize-hole" colspan="3">Jumlah Lulusan Terlacak dengan Waktu Tunggu Mendapatakan Pekerjaan</th>
@@ -168,44 +175,41 @@
         </tr>
         <tr>
             <td>TS-4</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+            <td>30</td>
+            <td>25</td>
+            <td>10</td>
+            <td>7</td>
+            <td>5</td>
+            <td>3</td>
         </tr>
         <tr>
             <td>TS-3</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+            <td>25</td>
+            <td>20</td>
+            <td>8</td>
+            <td>6</td>
+            <td>4</td>
+            <td>2</td>
         </tr>
          <tr>
             <td>TS-2</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+            <td>35</td>
+            <td>34</td>
+            <td>14</td>
+            <td>10</td>
+            <td>9</td>
+            <td>1</td>
          </tr>
         </tr>
     </table>
+    </div>
+
+    <div id="dayasaing"  >
     <h1><b>Daaya Saing Lulusan Program Diploma Tiga</b></h1>
     <table>
         <tr>
             <th class="w3-flat-belize-hole" rowspan="2">Tahun Lulus</th>
-            <th class="w3-flat-belize-hole" rowspan="2">Jumlah lulusan </th>row
+            <th class="w3-flat-belize-hole" rowspan="2">Jumlah lulusan </th>
             <th class="w3-flat-belize-hole" rowspan="2">Jumlah Lulusan Terlacak </th>
             <th class="w3-flat-belize-hole" colspan="3">Jumlah Lulusan Terlacak dengan Waktu Tunggu Mendapatakan Pekerjaan</th>
         </tr>
@@ -216,38 +220,31 @@
         </tr>
         <tr>
             <td>TS-4</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+            <td>20</td>
+            <td>12</td>
+            <td>6</td>
+            <td>2</td>
+            <td>3</td>
+
         </tr>
         <tr>
             <td>TS-3</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+            <td>13</td>
+            <td>6</td>
+            <td>4</td>
+            <td>1</td>
+            <td>1</td>
         </tr>
          <tr>
             <td>TS-2</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+            <td>25</td>
+            <td>15</td>
+            <td>7</td>
+            <td>3</td>
+            <td>5</td>
          </tr>
         </tr>
     </table>
+    </div>
 </body>
 </html>

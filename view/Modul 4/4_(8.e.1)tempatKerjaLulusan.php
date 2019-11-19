644ba3ea601bf8513dae1fffab76235789de9a65
Muhammad Prasasta <prasastaa@gmail.com>
2019-11-19 16:40:20 +0700

    [debug]

--- view/Modul 4/4_(8.e.1)tempatKerjaLulusan.php
+++ view/Modul 4/4_(8.e.1)tempatKerjaLulusan.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php include '../header.php' ?>
 <style type="text/css">
     body{
@@ -185,3 +186,192 @@
 </body>
 </html>
 <?php include '../footer.php' ?>
+=======
+<?php include '../header.php' ?>
+<style type="text/css">
+    body{
+        color: white;
+        
+    }
+
+    table, th, td{
+        border: 1px solid white;
+    }
+
+    .tabel1{
+        padding-left: 12px;
+    }
+
+
+</style>
+<body>
+    <div class="tabel1">
+    <table>
+    <h1>Tabel Tempat Kerja Lulusan</h1>
+        <tr>
+            <th rowspan="2">Tahun Lulus</th>
+            <th rowspan="2">Jumlah Lulusan</th>
+          <th rowspan="2">Jumlah Lulusan yang Terlacak</th>
+            <th colspan="3">Jumlah Lulusan Terlacak yang Bekerja<br> berdasarkan Tingkat/Ukuran Tempat<br> Kerja/Berwirausaha</th>
+        </tr>
+
+       
+
+        </tr>
+            <td><b>Lokal/<br> Wilayah/<br> Berwirausaha<br> tidak Berizin</td></b>
+            <td><b>Nasional/<br> Berwirausaha<br> Berizin</td></b>
+            <td><b>Multinasiona/<br> Internasional</td></b>
+        </tr>
+
+        <tr>
+            <th>1</th>
+            <th>2</th>
+            <th>3</th>
+            <th>4</th>
+            <th>5</th>
+            <th>6</th>
+        </tr>
+
+        <tr>
+                <th style="padding:5px;">TS-4</th>
+                <th> </th>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <th> </th>
+        </tr>
+
+        <tr>
+                <th style="padding:5px;">TS-3</th>
+                <th></th>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <th> </th>
+        </tr>
+
+        <tr>
+                <th style="padding:5px;">TS-2</th>
+                <th> </th>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <th> </th>
+        </tr>
+
+
+    <tr>
+        <th style="padding:5px;"><b>Jumlah</th></b>
+        <th> </th>
+        <th> </th>
+        <th> </th>
+        <th> </th>
+		<th> </th>
+
+    </tr>
+
+    </table>
+
+    <br>
+    <table>
+    <h1>Tabel Kepuasan Pengguna</h1>
+        <tr>
+            <th rowspan="2" style="text-align: center"><b>No.</b></th>
+            <th rowspan="2" style="text-align: center"><b>Jenis Kemampuan</b></th>
+            <th colspan="4" style="text-align: center"><b>Tingkat Kepuasan Pengguna (%)</b></th>
+            <th rowspan="2" style="text-align: center"><b>Rencana Tindak Lanjut oleh UPPS/PS</b></th>
+        </tr>
+        <tr>
+            <td><b>Sangat Baik</b></td>
+            <td><b>Baik</b></td>
+            <td><b>Cukup</b></td>
+            <td><b>Kurang</b></td>
+        </tr>
+        <tr>
+            <td style="text-align: center">1</td>
+            <td style="text-align: center">2</td>
+            <td style="text-align: center">3</td>
+            <td style="text-align: center">4</td>
+            <td style="text-align: center">5</td>
+            <td style="text-align: center">6</td>
+            <td style="text-align: center">7</td>
+        </tr>
+        <tr>
+            <td style="text-align: center">1</td>
+            <td>Etika</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+
+        </tr>
+        <tr>
+            <td style="text-align: center">2</td>
+            <td>Keahlian pada bidang ilmu (kompetensi utama)</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+
+        </tr>
+        <tr>
+            <td style="text-align: center">3</td>
+            <td>Kemampuan berbahasa asing</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td style="text-align: center">4</td>
+            <td>Penggunaan teknologi informasi</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td style="text-align: center">5</td>
+            <td>Kemampuan berkomunikasi</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td style="text-align: center">6</td>
+            <td>Kerjasama tim</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td style="text-align: center">7</td>
+            <td>Pengembangan diri</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td colspan="2" style="text-align: center"><b>Jumlah</b></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+    </div>
+</body>
+</html>
+<?php include '../footer.php' ?>
+>>>>>>> Modul4
